afb01b5f6258348d189baa9ddf6646cd82a1afa4
rubn <rucko24@gmail.com>
2024-07-01 16:44:19 +0200

    :bug: detect windows process with wmic

--- src/main/java/org/example/service/GetProcessAtRuntime.java
+++ src/main/java/org/example/service/GetProcessAtRuntime.java
@@ -16,25 +16,22 @@ import java.util.function.Predicate;
 public class GetProcessAtRuntime {
 
     private static final String TEST_JAR = "test.jar";
-    private static final String[] SEARCH_JAVAW_PROCESS_NAME_USING_WMIC = {"wmic", "Path," ,"win32_process", "Where", "\"CommandLine Like '%test.jar%'\""};
-    private static final String[] SEARCH_JAVAW_PROCESS_NAME_USING_PS_ELF = { "/bin/sh", "-c", "ps -elf | grep -v grep | grep 'test.jar'" };
 
-    public GetProcessAtRuntime() {
+    private static final String[] SEARCH_JAVAW_PROCESS_NAME_USING_WMIC = {"cmd", "/c", "wmic", "Path", "win32_process", "Where", "\"CommandLine Like '%test.jar%'\""};
 
-    }
+    private static final String[] SEARCH_JAVAW_PROCESS_NAME_USING_PS_ELF = {"/bin/sh", "-c", "ps -elf | grep -v grep | grep 'test.jar'"};
 
     public void execute() {
         final GetOperatingSystem osInfo = GetOperatingSystem.getOsInfo();
 
-        if(osInfo == GetOperatingSystem.WINDOWS) {
+        if (osInfo == GetOperatingSystem.WINDOWS) {
 
-            this.detectTestJarProcess((line) -> line.startsWith("javaw.exe") && line.contains(TEST_JAR), SEARCH_JAVAW_PROCESS_NAME_USING_WMIC);
+            this.detectTheJavaProcessByName(line -> line.startsWith("javaw.exe") && line.contains(TEST_JAR), SEARCH_JAVAW_PROCESS_NAME_USING_WMIC);
 
-        } else if(osInfo == GetOperatingSystem.LINUX) {
+        } else if (osInfo == GetOperatingSystem.LINUX) {
 
-            this.detectTestJarProcess((line) ->  line.contains(TEST_JAR), SEARCH_JAVAW_PROCESS_NAME_USING_PS_ELF);
-
-        } else if(osInfo == GetOperatingSystem.MAC) {
+            this.detectTheJavaProcessByName(line -> line.contains(TEST_JAR), SEARCH_JAVAW_PROCESS_NAME_USING_PS_ELF);
+        } else if (osInfo == GetOperatingSystem.MAC) {
 
             this.detectMacProcess();
         } else if (osInfo == GetOperatingSystem.FREEBSD) {
@@ -46,26 +43,27 @@ public class GetProcessAtRuntime {
 
     }
 
-    private void detectTestJarProcess(Predicate<String> predicate, String ...command) {
+    private void detectTheJavaProcessByName(Predicate<String> predicate, String... command) {
 
-        try(final InputStream inputStream = Runtime.getRuntime().exec(command).getInputStream();
-            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+        try (final InputStream inputStream = Runtime.getRuntime().exec(command).getInputStream();
+             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
             boolean isTestJarExists = bufferedReader.lines()
+                    .peek(System.out::println)
                     .filter(predicate)
                     .count() > 1;
 
-            if(!isTestJarExists) {
+            if (!isTestJarExists) {
                 new Thread(ClickMe::new).start();
             } else {
                 JOptionPane.showMessageDialog(null, "El proceso existe", "Error", JOptionPane.ERROR_MESSAGE);
             }
 
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 
     private void detectMacProcess() {
         JOptionPane.showMessageDialog(null, "Not implemented yet", "Information message", JOptionPane.INFORMATION_MESSAGE);
